0ac9914a9326642f12c456c7f7b18c2dd1f4b8cb
Tanakorn Pummala <65160166@go.buu.ac.th>
2026-05-04 22:33:08 +0700

    app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+
+import Dashboard from './pages/Admin/Dashboard'
+import ManageTasks from './pages/Admin/ManageTasks'
+import CreateTask from './pages/Admin/CreateTask'
+import ManageUsers from './pages/Admin/ManageUsers'
+
+import UserDashboard from './pages/User/UserDashboard'
+import MyTask from './pages/User/MyTask'
+import TaskDetails from './pages/User/TaskDetails'
+
+import PrivateRoute from './routes/PrivateRoute'
 
 const App = () => {
   return (
     <div>
-      App
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={['admin']} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/manage-tasks" element={<ManageTasks />} />            
+            <Route path="/admin/create-task" element={<CreateTask />} />
+            <Route path="/admin/manage-users" element={<ManageUsers />} />
+          </Route>
+          {/* User Routes */}
+          <Route element={<PrivateRoute allowedRoles={['user']} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTask />} />
+            <Route path="/user/task-details/:id" element={<TaskDetails />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/pages/User/TaskDetails.jsx
+++ src/pages/User/TaskDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Dashboard = () => {
+const TaskDetails = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Dashboard = () => {
   )
 }
 
-export default Dashboard
+export default TaskDetails
